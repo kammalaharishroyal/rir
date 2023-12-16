a8197a52979baf168cb9f426ece7e76685c8195b
harish <kammalaharish3304@gmail.com>
2023-12-16 11:46:20 +0530

    6th

--- src/App.js
+++ src/App.js
@@ -6,19 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header1">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+       
        <Home1/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
