9c8137426c60def5df1a1edfdcfd84eda918fbae
harish <kammalaharish3304@gmail.com>
2023-12-17 13:48:19 +0530

    12th

---
+++ src/rirprojectcomponents/Contact.js
@@ -0,0 +1,24 @@
+function Contact(){
+    return (
+        <div className="contact">
+            <div className="contactinner">
+                
+           <ul>
+           <h1>Contact</h1>
+            <li>Call us:9347779216</li>
+            <li>Mail at:kammalaharish23@gmail.com</li>
+            <li>Adress:<br/>kallipalli<br/>Nallacheruvu<br/>AP<br/>515571</li>
+            
+           </ul>
+           </div>
+        </div>
+
+
+
+
+
+
+
+    );
+}
+export default Contact;
