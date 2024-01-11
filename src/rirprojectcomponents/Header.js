5bef01e63094f929bf70a8097cb44f3fa800e2a1
harish <kammalaharish3304@gmail.com>
2024-01-11 23:38:47 +0530

    latest

--- src/rirprojectcomponents/Header.js
+++ src/rirprojectcomponents/Header.js
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Header(){
     return(
+        <>
 
         <header className='header'>
-        <h1>RIR Biryani</h1>
+        <h1>RIR Home Foods</h1>
+        
 
          </header>
+         <nav className="Header-nav">
+         <h2><Link to={'order'}>Order</Link></h2>
+         <h2>Contact</h2>
+         <h2>About</h2>
+         
+         </nav>
+         </>
     )
 }
 
